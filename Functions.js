dfb90bfd96fb4e922dcd0dbef441dc00b1197cc3
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-07-22 18:41:00 +0530

    Functions and their examples

---
+++ Functions.js
@@ -0,0 +1,52 @@
+//Functions
+//1.
+function showAlert()
+{
+console.log("Hello Js");
+
+}
+showAlert(); //Hello Js
+
+
+
+
+//2.
+function showNumber(num)
+{
+
+    console.log(num);
+}
+
+showNumber(1); //1
+
+
+
+
+
+//3.
+//The values that functions received its called as Parameters.
+function sum (x,y)
+{
+console.log(x + y);
+
+}
+//The values passed to the functions while calling it called as an Arguments.
+sum(10,10); //20
+
+
+
+
+
+
+//4.
+
+function multiply(a,b)
+{
+ return a * b;
+}
+
+let getResult = multiply(12,2);
+
+console.log(getResult);  //24
+
+
