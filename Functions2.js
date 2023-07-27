dc40f1652b8b9c6d3bed20a1690b11414e01e2c8
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-07-27 12:29:43 +0530

    Function expression understanding with examples

--- Functions2.js
+++ Functions2.js
@@ -124,17 +124,28 @@ let factorialTwo = function fact2(n)//1.let factorialTwo = function fact2(n) Thi
 let answer = 1; //2.let answer = 1;: This line initializes a variable answer and sets it a value 1. The variable answer will be used to store the factorial result.
 
 
-for(let i = 1; i <= n; i++)
+for(let i = 1; i <= n; i++)//for(let i = 1; i <= n; i++){ ... }: This is a for loop that starts with i as 1 and runs as long as i is less than or equal to the input number n. In each iteration of the loop, i is incremented by 1.
 {
 
-    answer = answer * i;
+    answer = answer * i; //answer = answer * i;: Within the for loop, this line multiplies the current value of answer by the value of i and stores the result back in the answer variable. This way, it keeps updating answer by multiplying it with the numbers from 1 to n.
 }
 
-return answer;
+return answer;//After the for loop finishes, the function returns the final value of ans, which represents the factorial of the input number n.
 
 };
 
-console.log(factorialTwo(6));
+console.log(factorialTwo(6));//This line calls the factorialTwo function with an input value of 6 and logs the result to the console.
+
+// Initially, ans is set to 1.
+// The for loop starts with i = 1 and multiplies ans by 1, so ans remains 1 (1 * 1).
+// The loop continues with i = 2, and now ans is updated to 1 * 2 = 2.
+// The loop continues with i = 3, and ans is updated to 2 * 3 = 6.
+// The loop continues with i = 4, and ans is updated to 6 * 4 = 24.
+// Finally, the loop continues with i = 5, and ans is updated to 24 * 5 = 120.
+// Finally, the loop continues with i = 6, and ans is updated to 120 * 6 = 720.
+// So, the function returns 24, which is the factorial of 4.
+// So, the function returns 120, which is the factorial of 5.
+// So, the function returns 720, which is the factorial of 6.
 
 
 
