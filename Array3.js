736146eb3223d24d1e93346e03ed0778fd0f8204
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-07-31 12:39:21 +0530

    New file added Array3.js with methods and their exmaples

---
+++ Array3.js
@@ -0,0 +1,22 @@
+//Array methods:-
+
+//Example Movie theatre line :-
+
+const theatreLine = ["Yash","Siddhi","Aai","Papa" ,"MyFamily"];
+console.log(theatreLine);//["Yash","Siddhi","Aai","Papa" ,"MyFamily"];
+
+//Push(): Method adds a new element at  the end of the array.
+theatreLine.push("GrandMother");
+theatreLine.push("GrandFather");
+theatreLine.push("Uncle");
+theatreLine.push("Aunt");
+theatreLine.push("Satwik");
+theatreLine.push("Didu");
+
+console.log(theatreLine);//["Yash","Siddhi","Aai","Papa" ,"MyFamily","GrandMother","GrandFather","Uncle","Aunt","Satwik","Didu"];
+
+
+
+
+//pop(): //Removes the element at the end of an array.
+theatreLine.pop();
