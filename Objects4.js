e70990024847e4993dcd4f6fc923091efc7c89a5
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-08-02 18:01:40 +0530

    //Arrays and Objects :- with examples

---
+++ Objects4.js
@@ -0,0 +1,30 @@
+//Arrays and Objects :-
+
+// Array:-
+let arr = [1,2,3,4,5];
+
+console.log(arr); // [1,2,3,4,5];
+
+
+
+console.log(arr["0"]); //1
+console.log(arr["1"]); //2
+console.log(arr["2"]); //3
+console.log(arr["3"]); //4
+console.log(arr["4"]); //5
+
+
+//Object:-
+
+let obj = 
+//Propert and value
+{
+ "0" : 1,
+ "1" : 2,
+ "2" : 3,
+ "3" : 4,
+ "4" : 5
+
+};
+
+console.log(obj);
