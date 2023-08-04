474f182d27f8e35b2c0e02ed6a2d08ddc955c5db
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-08-04 12:36:05 +0530

    Deep copy VS shallow copy , spread operator understood well

--- Timingevts3.js
+++ Timingevts3.js
@@ -1,6 +1,7 @@
 //Deep copy VS Shallow copy :-
 
 
+///1.With Objects :-
 let girlName =
 {
 name : "Susan",
@@ -83,9 +84,127 @@ console.log(boyDetails);
 
 
 
+///1.With Arrays :-
+
+let arr = [1,2,3,4,5,6];
+console.log(arr);       //[1,2,3,4,5,6];
+
+
+let arr2 = arr;
+console.log(arr2);    //[1,2,3,4,5,6];
+
+
+arr2.push(7);       //Push() method :- Adds the element at the end of an array.
+console.log(arr2); //
+
+
+console.log(arr);  //[1,2,3,4,5,6,7];
+
+
+
+let arr3 = [...arr];
+
+console.log(arr3); //[1,2,3,4,5,6,7];
+
+arr3.pop();
+console.log(arr3);//[1,2,3,4,5,6];
+
+arr3.unshift(0);
+console.log(arr3);//[0,1,2,3,4,5,6];
+
+
+arr3.shift();
+console.log(arr3);//[1,2,3,4,5,6];
+
+
+console.log(arr); //[1,2,3,4,5,6,7];
+
+
+
+ //Additional Notes : Deep Copy vs Shallow Copy:-
+
+//  Deep Copy
+// ● In Deep copy, The values that are copied in the new variable are copied and
+// disconnected from the original variable, which means if you try to change the
+// new variable, the original variable shouldn't have any changes.
+// ● For a primitive value, you use a simple assignment:
+// For e.g.
+let num = 5;
+let copiedValue = num;
+// And when you change the value of the copiedValue variable, the original value
+// remains the same.   
+
+copiedValue = 10;
+console.log(copiedValue);  //10.
+console.log(num); //5.
+
+
+// How to make a Deep copy of arrays or objects?
+// By Using Spread Operator
+// The spread operator (...) allows us to copy all or part of an existing array or object
+// into another array or object.
+
+
+let girl =
+{
+name : "susan",
+age : 15
+}
+
+console.log(`Before using spread operator`);
+console.log(girl);
+//{
+// name : "susan",
+// age : 15
+// }
+
+
+
+let copiedGirl = {...girl};
+
+copiedGirl.name = "Shanaya";
+console.log(`After using spread operator`);
+console.log(copiedGirl);
+//{
+// name : "Shanaya",
+// age : 15
+// }
+console.log(girl);
+//{
+// name : "susan",
+// age : 15
+// }
+
+
+// In this example, all values in the copiedGirl object are disconnected from the
+// original girl object. So "girl" and "copiedGirl" both are different objects.
+
+// Note: Spread operator can be used in many cases, like when we want to expand,
+// copy,concat with math object.
 
 
 
 
+// Shallow Copy:
+// ● In shallow copy, when we copy the original object into the new object, the new
+// object has the copy of the original object's memory address, which means
+// both objects point to the same memory address.
+// For e.g.
+let personOne = {name:"Rahul",age:20};
+let personTwo = personOne;
+// If you assign one object to another like person2 = person1, then the value of
+// ‘person1’ gets assigned to ‘person2’ and therefore, they both will point to the same
+// location.
 
-    
+console.log(personOne == personTwo); //True.
+console.log(personOne === personTwo); //True.
+
+
+
+// Now if you changed the person2  state property in an object, changes would be reflect back to the original
+// ‘person1’ object because they point to the same memory address.
+
+personTwo.state ="Maharashtra";
+console.log(personTwo);//{name:"Rahul",age:20,state:"Maharashtra"};
+console.log(personOne);//{name:"Rahul",age:20,state:"Maharashtra"};
+//It wont be changed the answer remains same.
