a12c3ce96e2b85f769cb509f1677fdb9aa918419
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-08-07 18:11:02 +0530

    Accessing button elements

--- Dom.js
+++ Dom.js
@@ -7,12 +7,47 @@ helloButton.addEventListener('click',function(){
   alert("Hello Javascript DOM")  
 });
 
+
+
+
 //Hello-button clickable:-
+//Using seperated function:-
+
+function sayHello(){
+ alert("You logged In");
+        
+};
+
+
+function sayGoodBye(){
+
+
+    alert("See you later!")
+};
 
 let helloButtonTwo = document.getElementById("btnTwo");
 
-helloButtonTwo.addEventListener("click",function(){
+helloButtonTwo.addEventListener("click",sayHello);
 
-alert("You logged In");
 
-});
+let goodByeButton = document.getElementById("btnTwo");
+
+goodByeButton.addEventListener("click",sayGoodBye);
+
+
+
+function logOutFn()
+{
+
+alert("You Logged Out")
+
+};
+
+
+
+let logOutBtn = document.getElementById("btnThree");
+
+logOutBtn.addEventListener("click",logOutBtn);
+
+// We are calling this function in html button id="btnThree" using onClick attribute.
+
