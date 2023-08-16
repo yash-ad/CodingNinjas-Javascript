246473e42c85671530b997b1c5a7df05f054109f
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-08-16 13:15:13 +0530

    forOf loop

---
+++ forOf.js
@@ -0,0 +1,20 @@
+//forOf:-
+
+
+
+let arrayNums = [1,2,3,4,5,6,7,8,9];
+
+
+// for(let i = 0; i <=arrayNums.length; i++)
+// {
+
+
+// console.log(arrayNums[i]);
+
+// };
+
+
+for(let i of arrayNums){
+
+    console.log(i);
+}
